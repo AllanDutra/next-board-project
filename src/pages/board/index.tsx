d7a255be314f86fc286c512df1ae14406e3a3d36
Allan Dutra Feitosa <allandutra@radinfo.com.br>
2022-12-19 09:11:11 -0300

    feat: aula 48. Registrando tarefa

--- src/pages/board/index.tsx
+++ src/pages/board/index.tsx
@@ -5,16 +5,55 @@ import { getSession } from "next-auth/client";
 import { FiCalendar, FiClock, FiEdit2, FiPlus, FiTrash } from "react-icons/fi";
 import { SupportButton } from "../../components/SupportButton";
 import styles from "./styles.module.scss";
+import { FormEvent, useState } from "react";
 
-export default function Board() {
+import firebase from "../../services/firebaseConnection";
+
+interface BoardProps {
+  user: {
+    id: string;
+    nome: string;
+  };
+}
+
+export default function Board({ user }: BoardProps) {
+  const [input, setInput] = useState("");
+
+  async function handleAddTask(e: FormEvent) {
+    e.preventDefault();
+
+    if (input === "") return alert("Preencha alguma tarefa!");
+
+    await firebase
+      .firestore()
+      .collection("tarefas")
+      .add({
+        created: new Date(),
+        tarefa: input,
+        userId: user.id,
+        nome: user.nome,
+      })
+      .then((doc) => {
+        console.log("CADASTRADO COM SUCESSO!");
+      })
+      .catch((err) => {
+        console.log("ERRO AO CADASTRAR: ", err);
+      });
+  }
+
   return (
     <>
       <Head>
         <title>Minhas tarefas - Board</title>
       </Head>
       <main className={styles.container}>
-        <form>
-          <input type="text" placeholder="Digite sua tarefa..." />
+        <form onSubmit={handleAddTask}>
+          <input
+            type="text"
+            placeholder="Digite sua tarefa..."
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
           <button type="submit">
             <FiPlus size={25} color="#17181f" />
           </button>
@@ -73,7 +112,14 @@ export const getServerSideProps: GetServerSideProps = async ({ req }) => {
       },
     };
 
+  const user = {
+    nome: session?.user.name,
+    id: session?.id,
+  };
+
   return {
-    props: {},
+    props: {
+      user,
+    },
   };
 };
