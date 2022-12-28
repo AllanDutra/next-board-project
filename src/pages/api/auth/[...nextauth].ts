58b5d5986c02b71417bfe7dc4059c51922d4c65b
allandutra <allandutrafeitosa14@gmail.com>
2022-12-27 21:38:39 -0300

    feat: class 59, valid donors

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,25 +1,45 @@
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
+
+import firebase from "../../../services/firebaseConnection";
+
 export const authOptions = {
   providers: [
     Providers.GitHub({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      scope: 'read:user'
+      scope: "read:user",
     }),
   ],
   callbacks: {
     async session(session, profile) {
       try {
+        const lastDonate = await firebase
+          .firestore()
+          .collection("users")
+          .doc(String(profile.sub))
+          .get()
+          .then((snapshot) => {
+            if (snapshot.exists) {
+              return snapshot.data().lastDonate.toDate();
+            } else {
+              return null; // Usuário não é apoiador
+            }
+          });
+
         return {
           ...session,
-          id: profile.sub
-        }
+          id: profile.sub,
+          vip: lastDonate ? true : false,
+          lastDonate,
+        };
       } catch {
         return {
           ...session,
-          id: null
-        }
+          id: null,
+          vip: false,
+          lastDonate: null,
+        };
       }
     },
     async signIn(user, account, profile) {
@@ -30,7 +50,7 @@ export const authOptions = {
         console.log("DEU ERRO: ", err);
         return false;
       }
-    }
-  }
+    },
+  },
 };
 export default NextAuth(authOptions);
